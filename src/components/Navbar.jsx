6982ff877dec12ef2798f1794d9fc56511de28ae
myschproj <150742169+myschproj@users.noreply.github.com>
2024-05-14 01:23:57 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className='flex justify-between px-10 py-2 items-center text-[1.2vw]'>
-        <h1 className='text-[3vw] font-semibold'>ochi</h1>
+        <h1 className='text-[3vw] font-semibold'>Итериум</h1>
         <div className='hidden gap-6 md:flex'>
             <a href="#">Итериум.WMS</a>
             <a href="#">Итериум.TMS</a>
