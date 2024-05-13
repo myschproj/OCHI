5ccf486c9d4961d2156704b47dcfd204b327121a
myschproj <150742169+myschproj@users.noreply.github.com>
2024-05-14 01:27:21 +0300

    Update Weare.jsx

--- src/components/Weare.jsx
+++ src/components/Weare.jsx
@@ -4,9 +4,9 @@ import { motion } from "framer-motion"
 export const Weare = () => {
   return (
     <div data-scroll data-scroll-section data-scroll-speed className=' z-100 w-full h-1/4 font-semibold flex overflow-hidden bg-[#004D43] text-[28vw] whitespace-nowrap text-white border-t-2 border-b-2 rounded-r-lg rounded-l-lg'>
-        <motion.h2 initial={{x:0}} animate={{x:"-100%"}} transition={{repeat: Infinity, ease: 'linear', duration: 10}} className='pr-4'>WE ARE OCHI</motion.h2>
-        <motion.h2 initial={{x:0}} animate={{x:"-100%"}} transition={{repeat: Infinity, ease: 'linear', duration: 10}} className='pr-4'>WE ARE OCHI</motion.h2>
-        <motion.h2 initial={{x:0}} animate={{x:"-100%"}} transition={{repeat: Infinity, ease: 'linear', duration: 10}} className='pr-4'>WE ARE OCHI</motion.h2>
+        <motion.h2 initial={{x:0}} animate={{x:"-100%"}} transition={{repeat: Infinity, ease: 'linear', duration: 10}} className='pr-4'>ИТЕРИУМ ОФИШИАЛ</motion.h2>
+        <motion.h2 initial={{x:0}} animate={{x:"-100%"}} transition={{repeat: Infinity, ease: 'linear', duration: 10}} className='pr-4'>ИТЕРИУМ ОФИШИАЛ</motion.h2>
+        <motion.h2 initial={{x:0}} animate={{x:"-100%"}} transition={{repeat: Infinity, ease: 'linear', duration: 10}} className='pr-4'>ИТЕРИУМ ОФИШИАЛ</motion.h2>
     </div>
   )
 }
