685ef326de3382018433470a99de57718f84d295
myschproj <150742169+myschproj@users.noreply.github.com>
2024-05-14 01:23:44 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,12 +5,13 @@ const Navbar = () => {
     <div className='flex justify-between px-10 py-2 items-center text-[1.2vw]'>
         <h1 className='text-[3vw] font-semibold'>ochi</h1>
         <div className='hidden gap-6 md:flex'>
-            <a href="#">Services</a>
-            <a href="#">Our work</a>
-            <a href="#">About us</a>
-            <a href="#">Insights</a>
+            <a href="#">Итериум.WMS</a>
+            <a href="#">Итериум.TMS</a>
+            <a href="#">Итериум.HRMS</a>
+            <a href="#">Итериум.CRM</a>
+            <a href="#">Итериум.Дев</a>
         </div>
-        <a href='#'>Contact Us</a>
+        <a href='#'>О нас</a>
     </div>
   )
 }
